394f872de9664d0fcde7194ae906ff362afd11c7
IPikush <59697676+IPikush@users.noreply.github.com>
2021-06-02 17:36:30 +0300

    added first homework

---
+++ mypackage/TaskFive.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class TaskFive {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, enter up limit and press Enter");
+        int up = in.nextInt();
+        System.out.println("Please, enter down limit and press Enter");
+        int down = in.nextInt();
+        //Math.random chooses random double from [0,1) default
+        int random = (int) (Math.random()*(up-down)) + down;
+        System.out.println(random);
+    }
+}
